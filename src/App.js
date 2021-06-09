7efb65df9b24c9090f8d88d0d96314f2dd16b67f
KituneUdon <kituneudon97@gmail.com>
2021-06-09 15:18:38 +0900

    RailwayのReact04を実施

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/dachshund/Dachshund_rabbit.jpg',
+  )
+
   return (
     <div>
       <header>Dog アプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img
-        src="https://images.dog.ceo/breeds/dachshund/Dachshund_rabbit.jpg"
-        alt="犬の画像"
-      />
+      <img src={dogUrl} alt="犬の画像" />
     </div>
   )
 }
