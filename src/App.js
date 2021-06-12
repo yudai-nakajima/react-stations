12290cb72ecef6af05b84aa341d9056bcefcd24f
yudai-nakajima <kituneudon97@gmail.com>
2021-06-12 12:38:43 +0900

    RailwayのReact07を実施

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // DO NOT DELETE
-
 import * as React from 'react'
 import './App.css'
 
@@ -11,24 +10,37 @@ export const App = () => {
   const [dogUrl, setDogUrl] = React.useState(
     'https://images.dog.ceo/breeds/dachshund/Dachshund_rabbit.jpg',
   )
+  const [isLoading, setIsLoading] = React.useState(false)
 
   const changeDogUrlAtRandom = () => {
-    fetch('https://dog.ceo/api/breeds/image/random').then(res => {
-      const result = res.json()
-      result.then(r => setDogUrl(r.message))
-    })
+    setIsLoading(true)
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(r => setDogUrl(r.message))
+      .catch(() => alert('画像URLの取得に失敗しました。'))
+      .finally(() => setIsLoading(false))
   }
 
   return (
-    <div>
-      <header>Dog アプリ</header>
-      <p>犬の画像を表示するサイトです</p>
-      <figure>
-        <img src={dogUrl} alt="犬の画像" />
-      </figure>
-      <button type="button" onClick={changeDogUrlAtRandom}>
-        更新
-      </button>
+    <div className="container">
+      <header className="header">
+        <h1 className="header_title">Dog アプリ</h1>
+      </header>
+      <div className="site_description_container">
+        <p className="site_description">犬の画像を表示するサイトです。</p>
+        <figure className="image_container">
+          <img className="image" src={dogUrl} alt="犬の画像" />
+        </figure>
+        <button
+          className="button"
+          type="button"
+          onClick={changeDogUrlAtRandom}
+          disabled={isLoading}
+        >
+          更新
+        </button>
+      </div>
     </div>
   )
 }
